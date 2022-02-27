d3d6f0c68f17feacb881badb23cda3e2b8d969eb
Marco Amato <linediconsine@gmail.com>
2022-02-27 20:53:16 +0100

    clean main.py

---
+++ app/main.py
@@ -0,0 +1,32 @@
+import pathlib
+
+from langdetect import detect_langs
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import (
+    FastAPI,
+    Request,
+)
+
+from app.interface import SourceText, Response
+
+BASE_DIR = pathlib.Path(__file__).parent
+
+app = FastAPI()
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+
+@app.get("/hello", response_class=HTMLResponse)
+def home_view(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+app = FastAPI(redoc_url="/", docs_url=None)
+
+
+@app.post('/detect_language',
+          responses={200: {'model': Response}},
+          description="Detect language used, return \n  supports 55 languages, response is in (ISO 639-1 codes):",
+          tags=[])
+def detect_language(data: SourceText):
+    return detect_langs(data.text)
